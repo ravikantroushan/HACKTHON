00d360abc6d9536a76ca230ec1369646ca6b68dd
ravikantroushan <125126526+ravikantroushan@users.noreply.github.com>
2023-11-04 23:09:20 +0530

    MovieDB

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,20 @@
+// import { Carousel } from "react-responsive-carousel";
+// import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+// const MovieCarousel = ({ movies }) => {
+//   return (
+//     <Carousel autoPlay infiniteLoop>
+//       {movies.map((movie) => (
+//         <div key={movie.id}>
+//           <img
+//             src={`https://image.tmdb.org/t/p/w780${movie.backdrop_path}`}
+//             alt={movie.title}
+//           />
+//           <p className="legend">{movie.title}</p>
+//         </div>
+//       ))}
+//     </Carousel>
+//   );
+// };
+
+// export default MovieCarousel;
